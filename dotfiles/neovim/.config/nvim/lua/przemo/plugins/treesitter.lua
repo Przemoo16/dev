f0e56445bd6eb96672dc33bbda3a2bcfc6dfd19d
Przemek Kanach <kanach16@gmail.com>
2026-05-01 12:46:51 +0200

    Fix plugins

--- dotfiles/neovim/.config/nvim/lua/przemo/plugins/treesitter.lua
+++ dotfiles/neovim/.config/nvim/lua/przemo/plugins/treesitter.lua
@@ -4,16 +4,6 @@ return {
 	build = ":TSUpdate",
 
 	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "lua", "vim" },
-			sync_install = false,
-			auto_install = true,
-			indent = {
-				enable = true,
-			},
-			highlight = {
-				enable = true,
-			},
-		})
+		require("nvim-treesitter").install({ "lua", "vim" })
 	end,
 }
--- tasks/neovim.sh
+++ tasks/neovim.sh
@@ -4,4 +4,5 @@ set -euo pipefail
 
 sudo pacman -S --needed --noconfirm neovim
 sudo pacman -S --needed --noconfirm ripgrep # Required by telescope
+sudo pacman -S --needed --noconfirm tree-sitter-cli # Required by nvim-treesitter
 sudo pacman -S --needed --noconfirm unzip # Required by stylua
